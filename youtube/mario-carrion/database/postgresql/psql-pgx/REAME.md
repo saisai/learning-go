688e20fad9c2da2a8e3b35a70631d910a68add0e
saisai <nyeinpeee@gmail.com>
2026-06-21 20:22:09 +0700

    first commit

---
+++ youtube/mario-carrion/database/postgresql/psql-pgx/REAME.md
@@ -0,0 +1,10 @@
+# Create table
+```
+create table users(name VARCHAR not null, birth_year smallint null default 0);
+INSERT INTO users(name, birth_year) VALUES('mario', 1900);
+SELECT * FROM users;
+go get github.com/jackc/pgx/v5
+docker exec -it container_id psql -U user -d dbname
+```
+
+Youtube [https://www.youtube.com/watch?v=2XCaKYH0Ydo&list=PL7yAAGMOat_EgwoQTvNUflrYL_4qzdB7f]
---
+++ youtube/mario-carrion/database/postgresql/psql-pgx/main.go
@@ -0,0 +1,99 @@
+package main
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"net/url"
+
+	_ "github.com/jackc/pgx/v5/stdlib"
+)
+
+func main() {
+
+	// ctx, cancel := context.Background()
+	// defer cancel()
+
+	dsn := url.URL{
+		Scheme: "postgres",
+		Host:   "localhost:5432",
+		User:   url.UserPassword("postgres", "postgres"),
+		Path:   "dbname",
+	}
+
+	q := dsn.Query()
+	q.Add("sslmode", "disable")
+
+	dsn.RawQuery = q.Encode()
+
+	db, err := sql.Open("pgx", dsn.String())
+	if err != nil {
+		fmt.Println("db.PingContext", err)
+		return
+	}
+
+	defer func() {
+		_ = db.Close()
+		fmt.Println("closed")
+	}()
+
+	if err := db.PingContext(context.Background()); err != nil {
+		fmt.Println("db.QueryRowContext", err)
+		return
+	}
+
+	row := db.QueryRowContext(context.Background(), "SELECT birth_year FROM users WHERE name = 'mario'")
+	if err != nil {
+		fmt.Println("db.QueryRowCotnext", err)
+		return
+	}
+
+	var birth_year int64
+
+	if err := row.Scan(&birth_year); err != nil {
+		fmt.Println("row.Scan", err)
+		return
+	}
+
+	fmt.Println("birth_year", birth_year)
+
+	// if _, err := db.ExecContext(context.Background(), "INSERT INTO users(name, birth_year) VALUES ('wario', 1901)"); err != nil {
+	// 	fmt.Println("db.ExecContext", err)
+	// 	return
+	// }
+
+	// name := "wario2"
+	// birth_year = 1903
+	// _, err = db.ExecContext(context.Background(), "INSERT INTO users(name, birth_year) VALUES($1, $2)", name, birth_year)
+	// if err != nil {
+	// 	fmt.Println("db.ExecContext", err)
+	// 	return
+	// }
+
+	rows, err := db.QueryContext(context.Background(), "SELECT name, birth_year from users")
+	if err != nil {
+		fmt.Println("row.Scan", err)
+		return
+	}
+
+	defer func() {
+		_ = rows.Close()
+	}()
+
+	if rows.Err() != nil {
+		fmt.Println("rows.Err()", err)
+		return
+	}
+
+	for rows.Next() {
+		var name string
+		var birth_year int64
+
+		if err = rows.Scan(&name, &birth_year); err != nil {
+			fmt.Println("rows.Scan", err)
+			return
+		}
+
+		fmt.Println("name", name, "birth_year", birth_year)
+	}
+}
